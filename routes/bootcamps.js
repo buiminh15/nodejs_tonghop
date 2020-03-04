8563ddd5c6f04fbb5fc7d57220624e0ddc74e393
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-03-04 17:47:55 +0700

    course routes controller

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,9 +1,31 @@
-const express = require('express')
-const router = express.Router()
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps')
+const express = require('express');
+const router = express.Router();
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius
+} = require('../controllers/bootcamps');
 
-router.route('/').get(getBootcamps).post(createBootcamp)
+// include other resource routers
+const courseRouter = require('./courses')
 
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter)
 
-module.exports = router
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router
+  .route('/')
+  .get(getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
